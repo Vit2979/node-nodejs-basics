be1dcf6b051a79b8c18fa8246ba57dd14049e450
Vit2979 <Kozikvit@gmail.com>
2023-06-19 23:44:26 +0300

    add subtask Hash

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,17 @@
+import { fileURLToPath } from "url";
+import { createReadStream } from "fs";
+import { stdout } from "process";
+import * as path from "path";
+
+const { createHash } = await import("crypto");
 const calculateHash = async () => {
-    // Write your code here 
+  const __filename = fileURLToPath(import.meta.url);
+  const __dirname = path.dirname(__filename);
+  const file = path.join(__dirname, "files", "fileToCalculateHashFor.txt");
+  const data = createHash("sha256");
+  const readStream = createReadStream(file);
+
+  readStream.pipe(data).setEncoding("hex").pipe(stdout);
 };
 
 await calculateHash();
