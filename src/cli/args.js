a0e748bf5759859eb5087e466c6f1367e9b71632
Vit2979 <Kozikvit@gmail.com>
2023-06-19 23:14:42 +0300

    add subtask command line interface

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,18 @@
 const parseArgs = () => {
-    // Write your code here 
+  let ars = "";
+  let isBool = true;
+  process.argv.forEach((element, i) => {
+    if (i > 1) {
+      if (isBool) {
+        ars += element;
+        isBool = false;
+      } else {
+        if (i % 2 === 0) ars += `, ${element}`;
+        else ars += ` is ${element}`;
+      }
+    }
+  });
+  if (ars) console.log(ars);
 };
 
 parseArgs();
--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,16 @@
 const parseEnv = () => {
-    // Write your code here 
+  let ars = "";
+  let isBool = true;
+  Object.entries(process.env).forEach(([key, value]) => {
+    if (key.substring(0, 4) === "RSS_") {
+      const element = `${key}=${value};`;
+      if (isBool) {
+        ars += element;
+        isBool = false;
+      } else ars += ` ${element}`;
+    }
+  });
+  if (ars) console.log(ars.slice(0, -1));
 };
 
 parseEnv();
