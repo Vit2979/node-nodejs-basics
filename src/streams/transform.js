65db30d65837f9c470bba9e04be16e95a7a7d59f
Vit2979 <Kozikvit@gmail.com>
2023-06-20 18:31:09 +0300

    add subtask Streams

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,5 +1,26 @@
+import { Transform } from "stream";
+import * as readline from "readline";
+
 const transform = async () => {
-    // Write your code here 
+  const readToLine = readline.createInterface({
+    input: process.stdin,
+  });
+
+  const startMessage = 'Enter your text or press "ctrl + c" to quit:\n';
+  process.stdout.write(startMessage);
+
+  const flipStream = new Transform({
+    transform(data) {
+      const flipData = data.split("").reverse().join("");
+      return flipData;
+    },
+  });
+
+  readToLine.on("line", async (line) => {
+    const message = await flipStream._transform(line);
+    process.stdout.write(message);
+    process.stdout.write(`\n${startMessage}`);
+  });
 };
 
 await transform();
--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,33 @@
+import { fileURLToPath } from "url";
+import * as readline from "readline";
+import fs from "fs";
+import * as path from "path";
+
 const write = async () => {
-    // Write your code here 
+  const __filename = fileURLToPath(import.meta.url);
+  const __dirname = path.dirname(__filename);
+  const fileToWrite = path.join(__dirname, "files", "fileToWrite.txt");
+  const writableStream = fs.createWriteStream(fileToWrite);
+
+  writableStream.on("error", (err) => {
+    console.log(
+      `An error occurred while writing to the file. Error: ${err.message}`
+    );
+  });
+
+  const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+    prompt: 'Enter your text or press "ctrl + c" to quit:\n',
+  });
+
+  rl.prompt();
+
+  rl.on("line", (line) => {
+    const offer = line + "\n";
+    writableStream.write(offer);
+    rl.prompt();
+  });
 };
 
 await write();
