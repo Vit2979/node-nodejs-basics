922b8424230cf9d43028d38c72e910f125ae0444
Vit2979 <Kozikvit@gmail.com>
2023-06-21 21:06:57 +0300

    add subtask Modules

--- src/modules/cjsToEsm.cjs
+++ src/modules/cjsToEsm.cjs
@@ -1,21 +1,34 @@
-const path = require('path');
-const { release, version } = require('os');
-const { createServer: createServerHttp } = require('http');
-require('./files/c');
+import { fileURLToPath } from 'url';
+import { release, version } from 'os';
+import { createServer as createServerHttp } from 'http';
+import './files/c.js';
+import { readFile } from 'fs/promises';
+import * as path from 'path';
 
 const random = Math.random();
 
 let unknownObject;
 
 if (random > 0.5) {
-    unknownObject = require('./files/a.json');
+  unknownObject = JSON.parse(
+    await readFile(
+      new URL('./files/a.json', import.meta.url)
+    )
+  );
 } else {
-    unknownObject = require('./files/b.json');
+  unknownObject = JSON.parse(
+    await readFile(
+      new URL('./files/b.json', import.meta.url)
+    )
+  );
 }
 
 console.log(`Release ${release()}`);
 console.log(`Version ${version()}`);
 console.log(`Path segment separator is "${path.sep}"`);
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 console.log(`Path to current file is ${__filename}`);
 console.log(`Path to current directory is ${__dirname}`);
@@ -24,16 +37,7 @@ const myServer = createServerHttp((_, res) => {
     res.end('Request accepted');
 });
 
-const PORT = 3000;
-
-console.log(unknownObject);
-
-myServer.listen(PORT, () => {
-    console.log(`Server is listening on port ${PORT}`);
-    console.log('To terminate it, use Ctrl+C combination');
-});
-
-module.exports = {
+export {
     unknownObject,
     myServer,
 };
