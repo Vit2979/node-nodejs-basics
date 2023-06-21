8bf17ab5c920512dd247810e83af972f679541f6
Vit2979 <Kozikvit@gmail.com>
2023-06-21 17:30:43 +0300

    add subtask Zlib

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,19 @@
-const compress = async () => {
-    // Write your code here 
+    import { fileURLToPath } from "url";
+    import fs from "fs";
+    import * as zlib from "zlib";
+    import * as path from "path";
+    
+    const compress = async () => {
+      const __filename = fileURLToPath(import.meta.url);
+      const __dirname = path.dirname(__filename);
+      const fileRead = path.join(__dirname, "files", "fileToCompress.txt");
+      const fileWrite = path.join(__dirname, "files", "archive.gz");
+    
+      const gz = zlib.createGzip();
+    
+      const readStream = fs.createReadStream(fileRead);
+      const writableStream = fs.createWriteStream(fileWrite);
+      readStream.pipe(gz).pipe(writableStream);
 };
 
 await compress();
