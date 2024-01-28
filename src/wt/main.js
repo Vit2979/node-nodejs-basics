9318d6ef1a7e258c64de3d22df0cf13e5048543d
Vit2979 <86978620+Vit2979@users.noreply.github.com>
2024-01-28 23:38:55 +0300

    add subtask Worker Threads (src/wt)

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,34 @@
-const performCalculations = async () => {
-    // Write your code here
+import * as os from 'os';
+import { Worker } from 'worker_threads';
+
+export const performCalculations = async () => {
+  const CPUCores = os.cpus();
+  const promiseArray = CPUCores.map((core, index) => {
+    let obj = {
+      status: '',
+      data: null,
+    };
+    
+    const worker = new Worker('./worker.js', {
+      workerData: 10 + index,
+    });
+    
+    return new Promise(resolve => {
+      worker.on('message', msg => {
+        obj.status = 'resolved';
+        obj.data = msg;
+        resolve(obj);
+      });
+      
+      worker.on('error', err => {
+        obj.status = 'error';
+        resolve(obj);
+      });
+    });
+  });
+
+  const outputArray = await Promise.all(promiseArray);
+  console.log(outputArray);
 };
 
-await performCalculations();
+performCalculations();
